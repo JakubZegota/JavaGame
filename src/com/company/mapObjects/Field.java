0a5da19a5ef8e8057fabc4ab446f0716c26e85b9
Janek <janekdomozych99@gmail.com>
2023-01-05 18:15:49 +0100

    Map

--- src/com/company/mapObjects/Field.java
+++ src/com/company/mapObjects/Field.java
@@ -1,7 +1,23 @@
 package com.company.mapObjects;
 
+import com.company.fieldObjects.Troop;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Field {
-    Coordinates coordinates;
+    private Coordinates coordinates;
+    private List<Troop> troops;
 
+    public Field(int x, int y) {
+        this.coordinates = new Coordinates(x, y);
+        troops = new LinkedList<>();
+    }
 
+    @Override
+    public String toString() {
+        return " {" +coordinates +
+                ", troops=" + troops +
+                '}';
+    }
 }
