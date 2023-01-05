ac6ddd21c30aad5b77788e54ace50b2f60875381
Janek <janekdomozych99@gmail.com>
2023-01-05 17:25:57 +0100

    Map

--- src/com/company/mapObjects/Map.java
+++ src/com/company/mapObjects/Map.java
@@ -1,4 +1,24 @@
 package com.company.mapObjects;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Map {
+    private int size_X; // lista kolumn
+    private int size_Y; //lista wierszy
+    private List<List<Field>> board;
+
+    public Map(int size_X, int size_Y) {
+        this.size_X = size_X;
+        this.size_Y = size_Y;
+        this.board = new ArrayList<>(size_Y);
+        for (var row: board) {
+            row = new ArrayList<>(size_X);
+        }
+    }
+
+    void display() {
+
+    }
 }
