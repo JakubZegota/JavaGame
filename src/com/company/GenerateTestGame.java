397f7d1e572033ee6acdd53f07df5c5a19390da8
Janek <janekdomozych99@gmail.com>
2023-01-05 18:15:49 +0100

    Map feature changes

--- src/com/company/GenerateTestGame.java
+++ src/com/company/GenerateTestGame.java
@@ -1,5 +1,15 @@
 package com.company;
 
-public class GenerateTestGame {
+import com.company.mapObjects.Map;
 
+public class GenerateTestGame {
+    private Map map;
+
+    public GenerateTestGame() {
+        this.map = new Map();
+    }
+
+    public Map getMap() {
+        return map;
+    }
 }
--- src/com/company/mapObjects/Map.java
+++ src/com/company/mapObjects/Map.java
@@ -12,16 +12,24 @@ public class Map {
         this.numberOfColumns = numberOfColumns;
         this.numberOfRows = numberOfColumns;
         this.board = new ArrayList<>(numberOfRows);
-        for (var row: board) {
-            row = new ArrayList<>(numberOfColumns);
+        for (int x = 0;x <numberOfRows; x++){
+            this.board.set(x, new ArrayList<>(numberOfColumns));
+            for (int y = 0; y < numberOfColumns; y++) {
+                this.board.get(x).set(y, new Field(x,y));
+            }
         }
+
     }
 
     public Map(){
         this(10, 10);
     }
 
-    void display() {
-
+    public void display() {
+        for (var row: board) {
+            for (var col: row) {
+                System.out.printf(col.toString());
+            }
+        }
     }
 }
